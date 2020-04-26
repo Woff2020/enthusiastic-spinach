8e8318d589c3f7819158cd23cbcda9d6e16f0a1c
assaf317 <assaf317@gmail.com>
2020-04-27 00:28:46 +0300

    added robots.txt  plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
         `gatsby-plugin-react-helmet`,
         `gatsby-source-data`,
         `gatsby-plugin-sitemap`,
+        'gatsby-plugin-robots-txt',
         {
             resolve: `gatsby-source-filesystem`,
             options: {
