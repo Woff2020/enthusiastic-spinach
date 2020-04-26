715e8128ebe6fda966b9c84a33e6c908c55fb6fe
assaf317 <assaf317@gmail.com>
2020-04-26 15:46:36 +0300

    fix whatsapp share

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -15,6 +15,7 @@ export default class Body extends React.Component {
                     <meta charSet="utf-8" />
                     <meta name="viewport" content="width=device-width, initialScale=1.0" />
                     <meta name="google" content="notranslate" />
+                    <meta property="og:image" content="images/icons/datomize192.png" />
                     <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:400,400i,700,700i" rel="stylesheet" />
                     <link rel="stylesheet" href={safePrefix('assets/css/main.css')} />
                     <link rel="shortcut icon" href="images/icons/favicon.ico" />
