cf8314852c0ab6c7bcb04dfa74a743bd2b2a22a5
assaf317 <assaf317@gmail.com>
2020-04-26 15:40:13 +0300

    update favicon

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import {Helmet} from 'react-helmet';
+import { Helmet } from 'react-helmet';
 import _ from 'lodash';
 
-import {safePrefix} from '../utils';
+import { safePrefix } from '../utils';
 import Header from './Header';
 import Footer from './Footer';
 
@@ -12,18 +12,20 @@ export default class Body extends React.Component {
             <React.Fragment>
                 <Helmet>
                     <title>{_.get(this.props, 'pageContext.frontmatter.title') && _.get(this.props, 'pageContext.frontmatter.title') + ' - '}{_.get(this.props, 'pageContext.site.siteMetadata.title')}</title>
-                    <meta charSet="utf-8"/>
+                    <meta charSet="utf-8" />
                     <meta name="viewport" content="width=device-width, initialScale=1.0" />
                     <meta name="google" content="notranslate" />
-                    <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:400,400i,700,700i" rel="stylesheet"/>
-                    <link rel="stylesheet" href={safePrefix('assets/css/main.css')}/>
+                    <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:400,400i,700,700i" rel="stylesheet" />
+                    <link rel="stylesheet" href={safePrefix('assets/css/main.css')} />
+                    <link rel="shortcut icon" href="images/icons/favicon.ico" />
+                    <link rel="apple-touch-icon" href="images/icons/datomize192.png" />
                 </Helmet>
                 <div id="page" className={'site palette-' + _.get(this.props, 'pageContext.site.siteMetadata.palette')}>
-                  <Header {...this.props} />
-                  <main id="content" className="site-content">
-                    {this.props.children}
-                  </main>
-                  <Footer {...this.props} />
+                    <Header {...this.props} />
+                    <main id="content" className="site-content">
+                        {this.props.children}
+                    </main>
+                    <Footer {...this.props} />
                 </div>
             </React.Fragment>
         );
